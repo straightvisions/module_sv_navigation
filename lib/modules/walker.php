cf27f8ff152315df73769b3b63d6cb37202a425e
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-03-29 13:23:55 +0100

    Added custom walker class

---
+++ lib/modules/walker.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * walker.php is a sample class for demonstrating PHPDoc
+ *
+ * @package     walker.php
+ * @author      straightvisions
+ * @version     1.0
+ * @access      public
+ * @since       7.3
+ * @see         https://straightvisions.com/
+ */
