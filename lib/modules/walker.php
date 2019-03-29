c29b26148238a6ddacd35cabc792a7be850dd6f5
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-03-29 13:24:20 +0100

    Added custom walker class

--- lib/modules/walker.php
+++ lib/modules/walker.php
@@ -1,11 +1,20 @@
 <?php
-/**
- * walker.php is a sample class for demonstrating PHPDoc
- *
- * @package     walker.php
- * @author      straightvisions
- * @version     1.0
- * @access      public
- * @since       7.3
- * @see         https://straightvisions.com/
- */
+namespace sv_100;
+
+class sv_navigation_walker extends \Walker_Nav_Menu {
+	function start_lvl( &$output, $depth = 0, $args = array() ) {
+
+	}
+
+	function end_lvl( &$output, $depth = 0, $args = array() ) {
+
+	}
+
+	function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
+
+	}
+
+	function end_el( &$output, $object, $depth = 0, $args = array() )  {
+
+	}
+}
