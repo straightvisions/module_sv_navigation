171021e376db18e4e7a45edccb6e926ed649dc08
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-03-31 21:21:21 +0200

    Added custom nav walker

--- lib/modules/walker.php
+++ lib/modules/walker.php
@@ -2,19 +2,71 @@
 namespace sv_100;
 
 class sv_navigation_walker extends \Walker_Nav_Menu {
-	function start_lvl( &$output, $depth = 0, $args = array() ) {
+	protected $init;
+	protected $show_images;
+
+
+	public function __construct( $show_images ) {
+		$this->init         = new init();
+		$this->show_images  = $show_images;
+	}
+
+	function start_lvl( &$output, $depth = 0, $args = array() ){
+		$indent     = str_repeat( "\t", $depth );
+		$submenu    = ( $depth > 0 ) ? ' sub-menu' : '';
+		$output     .= "\n$indent<ul class=\"sub-menu$submenu depth_$depth\">\n";
+	}
+
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ){
+		$indent         = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+		$li_attributes  = '';
+		$classes        = empty( $item->classes ) ? array() : (array) $item->classes;
+
+		$classes[]      = ( $args->walker->has_children ) ? 'dropdown' : '';
+		$classes[]      = ( $item->current || $item->current_item_ancestor ) ? 'active' : '';
+		$classes[]      = 'menu-item-' . $item->ID;
+
+		if( $depth && $args->walker->has_children ){
+			$classes[]  = 'dropdown-submenu';
+		}
+
+		$class_names    =  join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names    = ' class="' . esc_attr( $class_names ) . '"';
+
+		$id             = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args );
+		$id             = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output         .= $indent . '<li' . $id . $class_names . $li_attributes . '>';
+
+		$attributes     = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+		$attributes     .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+		$attributes     .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+		$attributes     .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
+
+		$attributes     .= ( $args->walker->has_children ) ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
+
+		$item_output    = $args->before;
+		$item_output    .= '<a' . $attributes . '>';
+
+		if ( $this->show_images && in_array( 'show_image', $classes ) && !$args->walker->has_children ) {
+			$item_output    .= get_the_post_thumbnail( $item->object_id, 'medium', array( 'alt' => esc_attr( $item->title ) ) );
+		}
+
+		$item_output    .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output    .= '</a>';
+		$item_output    .= $args->after;
+
+		$output .= apply_filters ( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 
 	}
 
-	function end_lvl( &$output, $depth = 0, $args = array() ) {
+	/*
+		function end_el(){ // closing li a span
 
-	}
+		}
 
-	function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
+		function end_lvl(){ // closing ul
 
-	}
-
-	function end_el( &$output, $object, $depth = 0, $args = array() )  {
-
-	}
+		}
+	*/
 }
