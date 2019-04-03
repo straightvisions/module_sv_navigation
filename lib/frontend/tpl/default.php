7b8d2e4055adfad325996dfb94eff49721fcd5ae
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-04-03 15:22:28 +0200

    Removed icon and moved too child module css

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -7,10 +7,5 @@ if ( has_nav_menu( $settings['location'] ) ) {
 		'walker'            => new sv_100\sv_navigation_walker( $settings['show_images'] ),
 	));
 
-	echo '<button type="button" class="' . $this->get_prefix( 'mobile_menu_toggle' ) . '">';
-	echo '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
-			<path d="M24 18v1h-24v-1h24zm0-6v1h-24v-1h24zm0-6v1h-24v-1h24z" fill="#1040e2"/>
-			<path d="M24 19h-24v-1h24v1zm0-6h-24v-1h24v1zm0-6h-24v-1h24v1z"/>
-			</svg>';
-	echo '</button>';
+	echo '<button type="button" class="' . $this->get_prefix( 'mobile_menu_toggle' ) . '"></button>';
 }
