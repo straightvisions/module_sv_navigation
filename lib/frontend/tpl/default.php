9c9465e16dc01f5257b5635bde685df2a8cb17e2
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-04-16 16:23:03 +0200

    Added shortcode setting class to add custom classes to navigation

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -1,9 +1,11 @@
 <?php
 if ( has_nav_menu( $settings['location'] ) ) {
+	$container_class        = $settings['class'] ? $this->get_prefix() . ' ' . $settings['class'] : $this->get_prefix();
+
 	wp_nav_menu( array(
 		'theme_location'	=> $settings['location'],
 		'depth'				=> $settings['depth'],
-		'container_class'	=> $this->get_prefix(),
+		'container_class'	=> $container_class,
 		'walker'            => new sv_100\sv_navigation_walker( $settings['show_images'] ),
 	));
 
