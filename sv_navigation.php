4c7efdeae12f4f2734ce847b44dd5c7e4258bdc0
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-11 20:55:23 +0200

    Added method to create menus and changed methods for creating nav location

--- sv_navigation.php
+++ sv_navigation.php
@@ -13,17 +13,18 @@ namespace sv_100;
 
 class sv_navigation extends init {
 	protected static $navs                      = array();
-	protected static $custom_scripts            = array();
-
+	protected static $menus						= array();
+	
 	// Properties
-	protected $location                         = false;
-	protected $description                      = false;
+	protected $nav								= array();
+	protected $menu								= array();
+	protected $old								= array();
 
 	public function __construct() {
 
 	}
 
-	public function init(){
+	public function init() {
 		// Translates the module
 		load_theme_textdomain( $this->get_module_name(), $this->get_path( 'languages' ) );
 
@@ -33,20 +34,21 @@ class sv_navigation extends init {
 
 		// Action Hooks
 		add_action( 'after_setup_theme', array( $this, 'register_navs' ) );
-
+		add_action( 'after_switch_theme', array( $this, 'nav_location_rescue' ) );
+		
 		// Shortcodes
 		add_shortcode( $this->get_module_name(), array( $this, 'shortcode' ) );
 
 		$this->load_modules();
 	}
 
-	protected function load_modules() :sv_navigation {
+	protected function load_modules(): sv_navigation {
 		require_once( $this->get_path( 'lib/modules/walker.php' ) );
 
 		return $this;
 	}
 
-	public function shortcode( $settings, $content = '' ) :string {
+	public function shortcode( $settings, $content = '' ): string {
 		$settings								= shortcode_atts(
 			array(
 				'location'						=> false,
@@ -66,48 +68,136 @@ class sv_navigation extends init {
 
 		return $output;
 	}
-
-	// Registers all created navigations
-	public function register_navs() {
-		register_nav_menus( static::$navs );
+	
+	// Tries to find menus in old locations and inserts them in new locations
+	public function nav_location_rescue() {
+		$old_theme 		= get_option( 'theme_switched' );
+		$old_theme_mods = get_option( 'theme_mods_' . $old_theme );
+		$old_theme_navs = isset( $old_theme_mods['nav_menu_locations'] ) ? $old_theme_mods['nav_menu_locations'] : false;
+		$new_theme_navs = ! empty( get_theme_mod( 'nav_menu_locations' ) ) ? get_theme_mod( 'nav_menu_locations' ) : false;
+		
+		if ( ! $new_theme_navs && ! $old_theme_navs ) {
+			$this->create_menus();
+		} else if ( ! $new_theme_navs && $old_theme_navs ) {
+			$new_theme_locations = get_registered_nav_menus();
+			
+			foreach ( $new_theme_locations as $location => $description ) {
+				$new_theme_navs[ $location ] = $old_theme_navs[ $location ];
+			}
+			
+			set_theme_mod( 'nav_menu_locations', $new_theme_navs );
+		}
 	}
 
-	// Object Methods
-	public function create( $parent ) :sv_navigation {
+	// Nav Methods
+	public function create( object $parent ): sv_navigation {
 		$new                                    = new static();
 
 		$new->set_root( $parent->get_root() );
 		$new->set_parent( $parent );
 
-		$new->location                          = $this->get_prefix( $parent->get_module_name() );
+		$new->nav['location']                  	= $this->get_prefix( $parent->get_module_name() );
 
 		return $new;
 	}
 
-	public function load_nav() :sv_navigation {
+	public function load_nav(): sv_navigation {
 		static::$navs[ $this->get_location() ]  = $this->get_desc() ? $this->get_desc() : $this->get_parent()->get_module_name();
 
 		return $this->get_root()->sv_navigation;
 	}
+	
+	// Registers all created navigations
+	public function register_navs() {
+		register_nav_menus( static::$navs );
+	}
 
-	// Setter & Getter
-	public function set_location( string $location ) :sv_navigation {
-		$this->location                         = $this->get_location() . '_' . $location;
+	// Nav - Setter & Getter
+	public function set_location( string $location ): sv_navigation {
+		$this->nav['location']                 = $this->get_location() . '_' . $location;
 
 		return $this;
 	}
 
-	public function get_location() :string {
-		return $this->location;
+	protected function get_location(): string {
+		return $this->nav['location'];
 	}
 
-	public function set_desc( string $description ) :sv_navigation {
-		$this->description                      = $description;
+	public function set_desc( string $description ): sv_navigation {
+		$this->nav['description']              = $description;
 
 		return $this;
 	}
 
-	public function get_desc() :string {
-		return $this->description;
+	protected function get_desc() :string {
+		return $this->nav['description'];
+	}
+	
+	// Menu Methods
+	public function create_menu( object $parent ): sv_navigation {
+		$new                                    = new static();
+		
+		$new->set_root( $parent->get_root() );
+		$new->set_parent( $parent );
+		
+		$new->menu['name']                 		= $this->get_prefix( $parent->get_module_name() );
+		$new->menu['location']					= $this->get_prefix( $parent->get_module_name() );
+		
+		return $new;
+	}
+	
+	public function load_menu(): sv_navigation {
+		if ( ! wp_get_nav_menu_object( $this->get_menu_name()  ) ) {
+			static::$menus[ $this->get_menu_name() ] = $this->menu;
+		}
+		
+		return $this->get_root()->sv_navigation;
+	}
+	
+	protected function create_menus(): sv_navigation {
+		foreach ( static::$menus as $name => $data ) {
+			$data['id'] = wp_create_nav_menu( $name );
+
+			if ( ! empty( $data['items'] ) ) {
+				foreach ( $data['items'] as $item ) {
+					wp_update_nav_menu_item( $data['id'], 0, $item );
+				}
+			}
+			
+			set_theme_mod( 'nav_menu_locations', array( $data['location'] => $data['id'] ) );
+		}
+		
+		return $this;
+	}
+	
+	// Menu - Setter & Getter
+	public function set_menu_name( string $name ): sv_navigation {
+		$this->menu['name'] = $name;
+		
+		return $this;
+	}
+	
+	protected function get_menu_name(): string {
+		return $this->menu['name'];
+	}
+	
+	public function set_menu_item( array $data ): sv_navigation {
+		$this->menu['items'][] = $data;
+		
+		return $this;
+	}
+	
+	protected function get_items(): array {
+		return $this->menu['items'];
+	}
+	
+	public function set_menu_location( string $location ): sv_navigation {
+		$this->menu['location'] = $this->menu['location'] . '_' . $location;
+		
+		return $this;
+	}
+	
+	protected function get_menu_location(): string {
+		return $this->menu['location'];
 	}
 }
