3ffbb364f4af5a352fec7aa3a80f0e1247ca1c82
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-06-26 15:43:49 +0200

    FIX NAMING

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -1,6 +1,6 @@
 <?php
 if ( has_nav_menu( $settings['location'] ) ) {
-	$menu = wp_nav_menu( array(
+	$nav_menu = wp_nav_menu( array(
 		'theme_location'	=> $settings['location'],
 		'depth'				=> $settings['depth'],
 		'container_class'	=> $settings['location'],
@@ -8,8 +8,8 @@ if ( has_nav_menu( $settings['location'] ) ) {
 		'walker'            => new sv_100\sv_navigation_walker( $settings['show_images'] ),
 	));
 	
-	if ( $menu ) {
-		echo $menu;
+	if ( $nav_menu ) {
+		echo $nav_menu;
 		echo '<button type="button" class="' . $this->get_prefix('mobile_menu_toggle') . ' ' . $settings['location'] . '_mobile_menu_toggle"></button>';
 	}
 }
