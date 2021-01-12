d2d0210fc3c2ab36785570536b269c836e231954
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-01-12 10:47:47 +0100

    FIX - ADD NOOPENER ATTRIBUTE WHEN TARGET IS SET

--- lib/modules/walker.php
+++ lib/modules/walker.php
@@ -35,7 +35,7 @@
 			$output         .= $indent . '<li' . $id . $class_names . $li_attributes . '>';
 	
 			$attributes     = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
-			$attributes     .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+			$attributes     .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '" rel="noopener"' : '';
 			$attributes     .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
 			$attributes     .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
 	
